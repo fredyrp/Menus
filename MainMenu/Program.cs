a7ec85f7aedff93e723afb11546b0862b833153d
Fredy Ramirez <fredy.rp@gmail.com>
2024-02-24 01:52:30 -0600

    MainMenu and SingleMenu is create.

---
+++ MainMenu/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using SingleMenu;
+
+namespace MainMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char key = ' ';
+            while (key != 'x')
+            {
+                Console.Clear();
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("--       Welcome to the Main Menu       --");
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("Select an menu:");
+                Console.WriteLine("\t1. Single Menu");
+                Console.WriteLine("\t2. Single Array Menu");
+                Console.WriteLine("\t3. Single Functions Menu");
+                Console.WriteLine("\tx. To exit");
+                Console.Write("Option: ");
+                key = Console.ReadKey().KeyChar;
+
+                Console.Clear();
+                switch (key)
+                {
+                    case '1':
+                        new MenuSingle().RunMenu();
+                        break;
+                    case '2':
+                        //SingleArrayMenu.MainMenu();
+                        break;
+                    case '3':
+                        //SingleFunctionsMenu.MainMenu();
+                        break;
+                    case 'x':
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ SingleMenu/MenuSingle.cs
@@ -0,0 +1,41 @@
+namespace SingleMenu
+{
+    public class MenuSingle
+    {
+        public void RunMenu()
+        {
+            char key = ' ';
+            while (key != 'x')
+            {
+                Console.Clear();
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("--  Welcome to the Single Console Menu  --");
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("  Select an option:");
+                Console.WriteLine("\t1. Option 1");
+                Console.WriteLine("\t2. Option 2");
+                Console.WriteLine("\t3. Option 3");
+                Console.WriteLine("\t4. Option 4");
+                Console.WriteLine("\tx. To exit");
+                Console.Write("  Option: ");
+                key = Console.ReadKey().KeyChar;
+
+                Console.Clear();
+
+                string message = $"You selected option {key}.";
+                if (key == 'x')
+                {
+                    message = "You selected to exit.";
+                }
+                else if (key < '1' || key > '4')
+                {
+                    message = "Invalid option.";
+                }
+
+                Console.WriteLine(message);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
